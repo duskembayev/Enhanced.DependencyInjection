55f147a426e513251abccaa25ccac7d00dc18e10
duskembayev <d.uskembayev@gmail.com>
2023-05-13 08:11:38 +0300

    introduce generic kinds of attributes

---
+++ src/Enhanced.DependencyInjection/ContainerEntryAttribute.Generic.cs
@@ -0,0 +1,79 @@
+﻿namespace Enhanced.DependencyInjection;
+
+/// <summary>
+///     Represents an attribute that is used to declare implementation entry lifetime and implemented services.
+/// </summary>
+/// <typeparam name="TInterface">
+///     The Type of the service.
+/// </typeparam>
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class ContainerEntryAttribute<TInterface> : Attribute
+{
+    /// <summary>
+    ///     Define implementation entry lifetime.
+    /// </summary>
+    /// <param name="lifetime">
+    ///     Specifies the lifetime of a service.
+    /// </param>
+    public ContainerEntryAttribute(ServiceLifetime lifetime)
+    {
+        Lifetime = lifetime;
+    }
+
+    internal ServiceLifetime Lifetime { get; }
+}
+
+/// <summary>
+///     Represents an attribute that is used to declare implementation entry lifetime and implemented services.
+/// </summary>
+/// <typeparam name="TInterface1">
+///     First type of the service.
+/// </typeparam>
+/// <typeparam name="TInterface2">
+///     Second type of the service.
+/// </typeparam>
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class ContainerEntryAttribute<TInterface1, TInterface2> : Attribute
+{
+    /// <summary>
+    ///     Define implementation entry lifetime.
+    /// </summary>
+    /// <param name="lifetime">
+    ///     Specifies the lifetime of a service.
+    /// </param>
+    public ContainerEntryAttribute(ServiceLifetime lifetime)
+    {
+        Lifetime = lifetime;
+    }
+
+    internal ServiceLifetime Lifetime { get; }
+}
+
+/// <summary>
+///     Represents an attribute that is used to declare implementation entry lifetime and implemented services.
+/// </summary>
+/// <typeparam name="TInterface1">
+///     First type of the service.
+/// </typeparam>
+/// <typeparam name="TInterface2">
+///     Second type of the service.
+/// </typeparam>
+/// <typeparam name="TInterface3">
+///     Third type of the service.
+/// </typeparam>
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class ContainerEntryAttribute<TInterface1, TInterface2, TInterface3> : Attribute
+{
+    /// <summary>
+    ///     Define implementation entry lifetime.
+    /// </summary>
+    /// <param name="lifetime">
+    ///     Specifies the lifetime of a service.
+    /// </param>
+    public ContainerEntryAttribute(ServiceLifetime lifetime)
+    {
+        Lifetime = lifetime;
+    }
+
+    internal ServiceLifetime Lifetime { get; }
+}
---
+++ src/Enhanced.DependencyInjection/SingletonAttribute.Generic.cs
@@ -0,0 +1,43 @@
+﻿namespace Enhanced.DependencyInjection;
+
+/// <summary>
+///     Attribute to declare implementation entry with singleton lifetime and implemented service.
+/// </summary>
+/// <typeparam name="TInterface">
+///     The Type of the service.
+/// </typeparam>
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class SingletonAttribute<TInterface> : Attribute
+{
+}
+
+/// <summary>
+///     Attribute to declare implementation entry with singleton lifetime and implemented services.
+/// </summary>
+/// <typeparam name="TInterface1">
+///     First type of the service.
+/// </typeparam>
+/// <typeparam name="TInterface2">
+///     Second type of the service.
+/// </typeparam>
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class SingletonAttribute<TInterface1, TInterface2> : Attribute
+{
+}
+
+/// <summary>
+///     Attribute to declare implementation entry with singleton lifetime and implemented services.
+/// </summary>
+/// <typeparam name="TInterface1">
+///     First type of the service.
+/// </typeparam>
+/// <typeparam name="TInterface2">
+///     Second type of the service.
+/// </typeparam>
+/// <typeparam name="TInterface3">
+///     Third type of the service.
+/// </typeparam>
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class SingletonAttribute<TInterface1, TInterface2, TInterface3> : Attribute
+{
+}
