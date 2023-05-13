b83468f5f27fb4eea9ddb86efb79f2b785ece71f
duskembayev <d.uskembayev@gmail.com>
2023-05-13 08:10:12 +0300

    introduce lifetime attributes

---
+++ src/Enhanced.DependencyInjection/ScopedAttribute.cs
@@ -0,0 +1,47 @@
+﻿namespace Enhanced.DependencyInjection;
+
+/// <summary>
+///     Attribute to declare implementation entry with scoped lifetime.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class ScopedAttribute : Attribute
+{
+    /// <summary>
+    ///     Define implemented services.
+    /// </summary>
+    public ScopedAttribute()
+    {
+        Interface0 = null;
+        Interfaces = Array.Empty<Type>();
+    }
+
+    /// <summary>
+    ///     Define implemented service.
+    /// </summary>
+    /// <param name="interface0">
+    ///     The Type of the service.
+    /// </param>
+    public ScopedAttribute(Type interface0)
+    {
+        Interface0 = interface0;
+        Interfaces = Array.Empty<Type>();
+    }
+
+    /// <summary>
+    ///     Define implemented services.
+    /// </summary>
+    /// <param name="interface0">
+    ///     The Type of the service.
+    /// </param>
+    /// <param name="interfaces">
+    ///     Additional types of the service.
+    /// </param>
+    public ScopedAttribute(Type interface0, params Type[] interfaces)
+    {
+        Interface0 = interface0;
+        Interfaces = interfaces;
+    }
+
+    internal Type? Interface0 { get; }
+    internal Type[] Interfaces { get; }
+}
---
+++ src/Enhanced.DependencyInjection/TransientAttribute.cs
@@ -0,0 +1,47 @@
+﻿namespace Enhanced.DependencyInjection;
+
+/// <summary>
+///     Attribute to declare implementation entry with transient lifetime.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class TransientAttribute : Attribute
+{
+    /// <summary>
+    ///   Define implemented services.
+    /// </summary>
+    public TransientAttribute()
+    {
+        Interface0 = null;
+        Interfaces = Array.Empty<Type>();
+    }
+
+    /// <summary>
+    ///   Define implemented service.
+    /// </summary>
+    /// <param name="interface0">
+    ///    The Type of the service.
+    /// </param>
+    public TransientAttribute(Type interface0)
+    {
+        Interface0 = interface0;
+        Interfaces = Array.Empty<Type>();
+    }
+
+    /// <summary>
+    ///     Define implemented services.
+    /// </summary>
+    /// <param name="interface0">
+    ///     The Type of the service.
+    /// </param>
+    /// <param name="interfaces">
+    ///     Additional types of the service.
+    /// </param>
+    public TransientAttribute(Type interface0, params Type[] interfaces)
+    {
+        Interface0 = interface0;
+        Interfaces = interfaces;
+    }
+
+    internal Type? Interface0 { get; }
+    internal Type[] Interfaces { get; }
+}
